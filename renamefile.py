185c6d2ffc8db9b377084991354ee09efa269547
Saurabh Jagtap <saurabh619@outlook.com>
2018-05-17 17:07:54 +0530

    Create renamefile.py

---
+++ renamefile.py
@@ -0,0 +1,21 @@
+'''Program to rename multiple files in a single folder with common naming system and incrementing number at the end'''
+
+import os
+def main():
+    i = 1
+    path = '' #set path = the path of the folder where you want to do the renaming task. Make sure its in the single quotes
+    stname = ''#set stname = the common characters/name you want for the files which wont change in every files of that folder
+    exten = ''#set exten = the extension of file you want to have.
+
+    #loop renaming the filenames
+    for filename in os.listdir(path):
+        dst = stname + str(i) + exten
+        src = path + '/' + filename
+        dst = path + '/' + dst
+        os.rename(src, dst)
+        i += 1
+
+# Driver Code
+if __name__ == '__main__':
+    # Calling main() function
+    main()
